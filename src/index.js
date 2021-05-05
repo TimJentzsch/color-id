3f9b8edd43559652385af2d550b68f09d1f0cf04
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-05-05 17:30:43 +0200

    Improve design of search input

--- src/index.js
+++ src/index.js
@@ -4,45 +4,56 @@ const PROJECT_VERSION = "0.0.1";
 const PROJECT_AUTHOR = "Tim Jentzsch";
 const PROJECT_SOURCE = "https://github.com/TimJentzsch/color-id";
 
+/** Gets the clostest colors to the input color. */
+function getNearestColors(input) {
+  const colors = Object.keys(culori.colorsNamed)
+    // Exclude duplicate colors
+    .filter((name) => !name.includes("gray"));
+  const nearest = culori.nearest(colors, culori.differenceCiede2000());
+
+  return nearest(input, 5).map((name) => {
+    return {
+      name,
+      hex: culori.formatHex(name),
+      style: `background-color: ${name};`,
+    };
+  });
+}
+
 const ColorIDApp = {
   data() {
+    // Start with a random color
+    const randomColor = culori.random();
+    const hex = culori.formatHex(randomColor);
+    const searchColor = {
+      hex,
+      style: `background-color: ${hex};`,
+    };
+    const nearestColors = getNearestColors(hex);
+
     return {
       name: PROJECT_NAME,
       version: PROJECT_VERSION,
       author: PROJECT_AUTHOR,
       source: PROJECT_SOURCE,
-      nearestColors: [],
+      colorInput: hex,
+      searchColor,
+      nearestColors,
     };
   },
+
   methods: {
     /** Identifies the given color input string. */
-    identifyColor(input) {
-      const colors = Object.keys(culori.colorsNamed);
-      const getNearestColors = culori.nearest(
-        colors,
-        culori.differenceCiede2000()
-      );
-
-      this.nearestColors = getNearestColors(input, 5).map((name) => {
-        return {
-          name,
-          hex: culori.formatHex(name),
-          style: `background-color: ${name};`,
+    identifyColor() {
+      const rgb = culori.converter("rgb")(this.colorInput);
+      if (rgb) {
+        const hex = culori.formatHex(rgb);
+        this.searchColor = {
+          hex,
+          style: `background-color: ${hex};`,
         };
-      });
-    },
-
-    /** Handles the submission of the identification form. */
-    onSubmitColorForm() {
-      const colorInput = document.getElementById("color-input");
-      if (colorInput) {
-        const color = colorInput.value;
-        this.identifyColor(color);
-      } else {
-        console.error("Color input not found!");
+        this.nearestColors = getNearestColors(hex);
       }
-      // Make sure that the page isn't reloaded
-      return false;
     },
   },
 };
