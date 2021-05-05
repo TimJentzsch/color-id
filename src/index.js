8dd9dcff3879be4ca3bac7522a942c1bc1a532b1
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-05-05 14:10:54 +0200

    Add color input form

--- src/index.js
+++ src/index.js
@@ -17,6 +17,27 @@ const ColorIDApp = {
 
 const app = Vue.createApp(ColorIDApp);
 
+/** Handles the submission of the identification form. */
+function onSubmitColorForm() {
+  const colorInput = document.getElementById("color-input");
+  if (colorInput) {
+    const color = colorInput.value;
+    console.info(`Identifying color ${color}`);
+  } else {
+    console.error("Color input not found!");
+  }
+  // Make sure that the page isn't reloaded
+  return false;
+}
+
+// Load the Vue app and register event listeners
 document.addEventListener("DOMContentLoaded", () => {
   app.mount("#app");
+
+  const colorForm = document.getElementById("color-input-form");
+  if (colorForm) {
+    colorForm.onsubmit = onSubmitColorForm;
+  } else {
+    console.error("Color form not found!");
+  }
 });
