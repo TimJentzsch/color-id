1204c848a986b55e836aa8e69445c72e305f1fa8
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-05-05 14:19:16 +0200

    Add basic nearest color identification

--- src/index.js
+++ src/index.js
@@ -17,12 +17,21 @@ const ColorIDApp = {
 
 const app = Vue.createApp(ColorIDApp);
 
+/** Identifies the given color input string. */
+function identifyColor(input) {
+  const colors = Object.keys(culori.colorsNamed);
+  const getNearestColors = culori.nearest(colors, culori.differenceCiede2000());
+
+  const nearestColors = getNearestColors(input, 5);
+  console.info(JSON.stringify(nearestColors));
+}
+
 /** Handles the submission of the identification form. */
 function onSubmitColorForm() {
   const colorInput = document.getElementById("color-input");
   if (colorInput) {
     const color = colorInput.value;
-    console.info(`Identifying color ${color}`);
+    identifyColor(color);
   } else {
     console.error("Color input not found!");
   }
