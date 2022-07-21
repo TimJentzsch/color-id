f3d53bd0fea9de7a38afa814c9f930f77bf3b13c
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-07-21 20:43:21 +0200

    Add component for a hue slider

--- src/lib/compositions/HueSlider.svelte
+++ src/lib/compositions/HueSlider.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	export let id: string | undefined = undefined;
-	export let value: number;
-	export let min: number = 0;
-	export let max: number = 100;
+	export let hue: number;
 	export let onChange: ((value: number) => void) | undefined = undefined;
 	export let onCompleteChange: ((value: number) => void) | undefined = undefined;
-	export let hueName: string;
 
-	$: percentage = ((value - min) / (max - min)) * 100.0;
+	$: percentage = ((hue % 360) / 360) * 100.0;
 
 	function internalOnChange(val: number) {
 		if (onChange !== undefined) {
@@ -15,24 +12,16 @@
 		}
 	}
 
-	$: internalOnChange(value);
+	$: internalOnChange(hue);
 
 	function internalOnCompleteChange() {
 		if (onCompleteChange !== undefined) {
-			onCompleteChange(value);
+			onCompleteChange(hue);
 		}
 	}
 </script>
 
-<input
-	{id}
-	style="--slider-hue: var(--{hueName}-hue); --slider-percentage: {percentage}%"
-	type="range"
-	bind:value
-	{min}
-	{max}
-	on:change={internalOnCompleteChange}
-/>
+<input {id} type="range" bind:value={hue} min={0} max={360} on:change={internalOnCompleteChange} />
 
 <style>
 	input[type='range'] {
@@ -44,10 +33,17 @@
 		cursor: col-resize;
 		background: linear-gradient(
 			to right,
-			hsla(var(--slider-hue), 80%, var(--lightness), 1) 0%,
-			hsla(var(--slider-hue), 80%, var(--lightness), 1) var(--slider-percentage),
-			hsla(var(--slider-hue), 80%, var(--lightness), 0.4) var(--slider-percentage),
-			hsla(var(--slider-hue), 80%, var(--lightness), 0.4) 100%
+			hsl(0, 80%, var(--lightness)) 0%,
+			hsl(36, 80%, var(--lightness)) 10%,
+			hsl(72, 80%, var(--lightness)) 20%,
+			hsl(108, 80%, var(--lightness)) 30%,
+			hsl(144, 80%, var(--lightness)) 40%,
+			hsl(180, 80%, var(--lightness)) 50%,
+			hsl(216, 80%, var(--lightness)) 60%,
+			hsl(252, 80%, var(--lightness)) 70%,
+			hsl(288, 80%, var(--lightness)) 80%,
+			hsl(324, 80%, var(--lightness)) 90%,
+			hsl(360, 80%, var(--lightness)) 100%
 		);
 	}
 
@@ -57,7 +53,7 @@
 		outline: solid var(--text-color) 2px;
 		border: solid var(--background-color) 2px;
 		border-radius: 0;
-		background-color: hsl(var(--slider-hue), 80%, var(--lightness));
+		background-color: transparent;
 		width: 3px;
 		height: 14px;
 		text-align: center;
