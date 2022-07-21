49e1dd56167b597970bf0db2a8ea80854b6f83c3
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-07-21 11:23:53 +0200

    Add basic page sections

---
+++ src/lib/sections/SvgNames.svelte
@@ -0,0 +1,1 @@
+<h2>SVG Color Names</h2>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Accessibility from '$lib/sections/Accessibility.svelte';
+	import Composition from '$lib/sections/Composition.svelte';
+	import Selection from '$lib/sections/Selection.svelte';
+	import SvgNames from '$lib/sections/SvgNames.svelte';
+</script>
+
+<Selection />
+
+<Composition />
+
+<SvgNames />
+
+<Accessibility />
