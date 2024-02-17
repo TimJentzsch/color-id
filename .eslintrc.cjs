75182121bc522f17b6ec6cd5cca3758d21951103
Tim Jentzsch <commits@timjen.net>
2024-02-17 15:09:12 +0100

    Update eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: ['plugin:svelte/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [{
+		files: ['*.svelte'], parser: 'svelte-eslint-parser', parserOptions: {
+			parser: '@typescript-eslint/parser'
+		}
+	}],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
+		project: '.svelte-kit/tsconfig.json',
+		extraFileExtensions: ['.svelte'],
 		ecmaVersion: 2020
 	},
 	env: {
