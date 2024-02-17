943d5b79bda325002a680e3e40b51b5f17c6b52c
TimJentzsch <commits@timjen.net>
2024-02-17 15:15:52 +0100

    Update dependencies (#12)
    * Run npm update to remove vulnerabilities
    
    * Update typescript dependencies
    
    * Update to Svelte 4
    
    * Update svelte kit
    
    * Update eslint plugin
    
    * Use TS for svelte config
    
    * Run prettier
    
    * Fix eslint config
    
    * Fix Svelte parsing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,25 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['plugin:svelte/recommended', 'prettier'],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
+		project: './tsconfig.json',
+		extraFileExtensions: ['.svelte'],
 		ecmaVersion: 2020
 	},
 	env: {
